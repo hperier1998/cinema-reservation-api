a3724ff6230fa6a02ab0bd97210436e7152ca128
RayZiaX <jeremy.magne13121@gmail.com>
2024-06-15 04:10:22 +0200

    routes-cinema - Add many routes - Update README - Add authors into README - Add Create table for rooms, seances and cinemas

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const movieRoutes = require('./movie');
 const reservationRoutes = require('./reservations');
+const cinemaRoutes = require('./cinemas')
 
 router.use('/movie', movieRoutes);
 router.use('/reservations', reservationRoutes);
-
+router.use('/cinema', cinemaRoutes)
 module.exports = router;
